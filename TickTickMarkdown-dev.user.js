873601d98b75320aa89bdb704c23964fb458a574
Dan Keder <dan.keder@gmail.com>
2019-03-04 13:33:58 +0100

    Add "www.ticktick.com" to @match

--- TickTickMarkdown-dev.user.js
+++ TickTickMarkdown-dev.user.js
@@ -5,6 +5,7 @@
 // @description  Add markdown support to TickTick (stub for local development)
 // @author       dan.keder@gmail.com
 // @match        https://ticktick.com/*
+// @match        https://www.ticktick.com/*
 // @grant        GM_addStyle
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/showdown/1.8.6/showdown.min.js
